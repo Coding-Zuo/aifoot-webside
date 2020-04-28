2aa99b0742cd4fd7724fb9ae9939176af6d1c945
Coding-Zuo <zzuuoo666@sina.com>
2020-04-28 12:11:48 +0800

    navbar删除两个按钮

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -8,13 +8,13 @@
       <template v-if="device!=='mobile'">
         <search id="header-search" class="right-menu-item" />
 
-        <el-tooltip content="源码下载" effect="dark" placement="bottom">
-          <Github class="right-menu-item hover-effect" />
-        </el-tooltip>
+<!--        <el-tooltip content="源码下载" effect="dark" placement="bottom">-->
+<!--          <Github class="right-menu-item hover-effect" />-->
+<!--        </el-tooltip>-->
 
-        <el-tooltip content="项目文档" effect="dark" placement="bottom">
-          <Doc class="right-menu-item hover-effect" />
-        </el-tooltip>
+<!--        <el-tooltip content="项目文档" effect="dark" placement="bottom">-->
+<!--          <Doc class="right-menu-item hover-effect" />-->
+<!--        </el-tooltip>-->
 
         <el-tooltip content="全屏缩放" effect="dark" placement="bottom">
           <screenfull id="screenfull" class="right-menu-item hover-effect" />
